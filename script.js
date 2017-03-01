b5bb3ef2b0330430189a203993772c34707a183d
Sean <sean.a.blackman@student.uncommonhs.org>
2017-03-01 21:59:04 +0000

    Boo

--- script.js
+++ script.js
@@ -1,1 +1,9 @@
 //Write your function calls below this line
+setColor(0,0, 'tomato');
+setColor(0, 9, 'blue');
+setColor(9, 0, 'green');
+setColor(9, 9, 'purple');
+
+setColor(3, 2, 'blue');
+setColor(3, 6, 'blue');
+setColor(5, 2, 'pink');
